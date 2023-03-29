4b4f1d8eb56b5a4ace7da5536c7f49ffe34618b9
renwire <104647150+renwire@users.noreply.github.com>
2023-03-28 21:41:37 -0500

    Update Program.cs

--- chatbot-with-openai/Program.cs
+++ chatbot-with-openai/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace ChatGPTExample
 {
@@ -12,38 +13,72 @@ namespace ChatGPTExample
     {
         static async Task Main(string[] args)
         {
-            string keyLocation = "/path/to/file.txt";
+            //store the the api key in a string from the directory location
+            string keyLocation = "D:\\Coding\\Projects\\Starbright\\C#\\chatbot-with-openai\\chatbot-with-openai\\resources\\Starbright\\APIKey.txt";
             string apiKey = File.ReadAllText(keyLocation);
-            string aiVersion = "/path/to/file.txt";
+            //Bring prompt from the directory location and store it in a string
+            string aiVersion = "D:\\Coding\\Projects\\Starbright\\C#\\chatbot-with-openai\\chatbot-with-openai\\resources\\Starbright\\SB v0.2.5.txt";
             string prompt = File.ReadAllText(aiVersion);
-
+            //get api instructions from the directory location and store it in a string
+            string instructionSet = "D:\\Coding\\Projects\\Starbright\\C#\\chatbot-with-openai\\chatbot-with-openai\\resources\\Starbright\\instructions.txt";
+            string instructions = File.ReadAllText(instructionSet);
+            //Create a new http client
             HttpClient httpClient = new HttpClient();
+
+            //put api site in string
             string baseUrl = "https://api.openai.com/v1/completions";
 
+            //Create a bool to continue the chat
             bool continueChat = true;
 
-            StreamWriter logFile = new StreamWriter("log.txt", true);
+            //Create a log file
+            StreamWriter logFile = new StreamWriter("D:\\Coding\\Projects\\Starbright\\C#\\chatbot-with-openai\\chatbot-with-openai\\resources\\Starbright\\log.txt", true);
 
+            // The maximum number of interactions you want to store
+            int maxInteractionCount = 1000; 
+
+            List<string> interactionHistory = new List<string>();
+
+            //chat loop
             while (continueChat)
             {
+                //Get user input to send to the API
                 Console.WriteLine("Please enter your prompt or type 'exit' to quit:");
-                prompt += "\n User:" + Console.ReadLine();
 
-                if (prompt.ToLower() == "exit")
+                string userInput = Console.ReadLine();
+
+                //Exit the chat if the user types 'exit'
+                if (userInput.ToLower() == "exit")
                 {
                     continueChat = false;
                     break;
                 }
 
+                //append the user input to the interactionHistory
+                interactionHistory.Add("User: " + userInput);
+
+                if (interactionHistory.Count > maxInteractionCount * 2)
+                {
+                    interactionHistory.RemoveAt(0);
+                    interactionHistory.RemoveAt(0);
+                }
+
+                prompt = File.ReadAllText(aiVersion) + "\n" + string.Join("\n", interactionHistory);
+
+                // Load instructions from the instruction set file
+                JObject instructionData = JObject.Parse(instructions);
+
+                //Send the request to the API
                 var requestData = new
                 {
-                    model = "text-davinci-003",
+                    model = instructionData["model"],
                     prompt = prompt,
-                    temperature = 0.7,
-                    max_tokens = 256,
-                    top_p = 1,
-                    frequency_penalty = 0,
-                    presence_penalty = 0
+                    temperature = instructionData["temperature"],
+                    max_tokens = instructionData["max_tokens"],
+                    top_p = instructionData["top_p"],
+                    best_of = instructionData["best_of"],
+                    frequency_penalty = instructionData["frequency_penalty"],
+                    presence_penalty = instructionData["presence_penalty"]
                 };
 
                 var jsonRequest = JsonConvert.SerializeObject(requestData);
@@ -63,24 +98,19 @@ namespace ChatGPTExample
                 var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
 
                 JObject responseData = JsonConvert.DeserializeObject<JObject>(jsonResponse);
-                string responseText = responseData["choices"][0]["text"].ToString();
+                string responseText = responseData["choices"][0]["text"].ToString().Trim();
 
                 Console.WriteLine($"Starbright: {responseText}\n");
 
-                // Write the user's response and AI's response to the log file
-                logFile.WriteLine($"User: {prompt}");
-                logFile.WriteLine($"Starbright: {responseText}\n");
+                interactionHistory.Add("Starbright: " + responseText);
 
-                // Reset the prompt for the next iteration
-                prompt = File.ReadAllText(aiVersion);
+                // Write the user's response and AI's response to the log file
+                logFile.WriteLine($"User: {userInput}");
+                logFile.WriteLine($"Starbright: {responseText}\n");
             }
 
             // Close the log file
-
-            // figure out how to add this to the SB v0.2.5.txt instead of creating a new file
-
             logFile.Close();
         }
     }
 }
-
